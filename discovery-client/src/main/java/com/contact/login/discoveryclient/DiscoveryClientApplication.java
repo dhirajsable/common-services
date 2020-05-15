0b60c3018a3e3cfe2e67b9de6c0b5916861e02cb
Dhiraj Sable <dhiraj.sable@glassdoor.com>
2020-05-15 16:28:16 -0700

    Added a docker config.

--- discovery-client/src/main/java/com/contact/login/discoveryclient/DiscoveryClientApplication.java
+++ discovery-client/src/main/java/com/contact/login/discoveryclient/DiscoveryClientApplication.java
@@ -1,13 +1,31 @@
 package com.contact.login.discoveryclient;
 
+import com.netflix.appinfo.InstanceInfo;
+import com.netflix.discovery.EurekaClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class DiscoveryClientApplication {
+
+    @Qualifier("eurekaClient")
+    @Autowired
+    EurekaClient eurekaClient;
+
+    @Autowired
+    RestTemplateBuilder restTemplateBuilder;
 
     public static void main(String[] args) {
         SpringApplication.run(DiscoveryClientApplication.class, args);
     }
-
 }
