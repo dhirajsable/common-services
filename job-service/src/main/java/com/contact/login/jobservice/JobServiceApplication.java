671bf4159959aa44145573cb6febe46f5ba94b62
Dhiraj Sable <dhiraj.sable@glassdoor.com>
2020-05-29 16:30:39 -0700

    Added a vault and config-service integration.

---
+++ job-service/src/main/java/com/contact/login/jobservice/JobServiceApplication.java
@@ -0,0 +1,14 @@
+package com.contact.login.jobservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
+
+@SpringBootApplication
+public class JobServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JobServiceApplication.class, args);
+    }
+
+}
